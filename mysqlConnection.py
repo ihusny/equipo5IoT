59601b73659e38e84dd94a13b97fb9b3bd7d0682
iArturoSosaC <iA01022687@itesm.mx>
2020-11-10 13:33:22 -0600

    Añadir datos a los perfiles

--- mysqlConnection.py
+++ mysqlConnection.py
@@ -1,45 +1,64 @@
 import mysql.connector
+import random
 from os import system, name
+
+def crearPerfil(cnx, cursor):
+    system('clear')
+    nombre = input("Introduzca su nombre: ")
+    apellido = input("Introduzca su apellido: ")
+    edad = input("Introduzca su edad: ")
+    query_data = (nombre, apellido, edad)
+    query = f"INSERT INTO Perfiles(nombre, apellido, edad) values (%s, %s, %s);"
+    cursor.execute(query, query_data)
+    cnx.commit()
 
 try:
     cnx = mysql.connector.connect(user='root', password='0412M4sql.', host='127.0.0.1', database='reto_sensor')
 
     cursor = cnx.cursor()
-    # perfiles = [
-    #             "1, 'Arturo', 'Sosa', 21);",
-    #             "2, 'Jorge', 'Becker', 20);",
-    #             "3, 'Isac', 'Husny', 21);",
-    #             ]
+
+
+    limit = int(input("¿Cuantos usuarios quieres agregar? "))
+
+    if (limit > 0):
+        for i in range(limit):
+            crearPerfil(cnx, cursor)
+
+# for i in range(5, 7):
+#     for j in range(1000):
+#         query = f"INSERT INTO Sensor_cardio(id_perfil, medicion_cardio, fecha) values("+ str(i)+ ", "+str(random.randint(50, 120))+", '2020-11-10');"
+#         cursor.execute(query)
+#         cnx.commit()
+#         query = f"INSERT INTO Sensor_oxi(id_perfil, medicion_oxi, fecha) values("+str(i)+", "+str(random.randint(70, 100))+", '2020-11-10');"
+#         cursor.execute(query)
+#         cnx.commit()
+
+
+    for i in range(5, 7):
+        for j in range(1000):
+
+            query_data = (i, str(random.randint(50, 120)), '2020-11-10')
+            query = f"INSERT INTO Sensor_cardio(id_perfil, medicion_cardio, fecha) values(%s, %s, %s);"
+            cursor.execute(query, query_data)
+            cnx.commit()
+
+            query_data = (i, str(random.randint(70, 100)), '2020-11-10')
+            query = f"INSERT INTO Sensor_oxi(id_perfil, medicion_oxi, fecha) values(%s, %s, %s);"
+            cursor.execute(query, query_data)
+            cnx.commit()
+
+
+    # last_id = [result[0] for result in cursor][-1]
+    # limit = input("¿Cuantos usuarios quieres agregar? ")
+    # for id_perfil in range(last_id+1, last_id+limit):
+    #     system('clear')
+    #     name = input("Introduzca su nombre: ")
+    #     apellido = input("Introduzca su apellido: ")
+    #     edad = input("Introduzca su edad: ")
+    #     query_data = (id_perfil, name, apellido, edad)
+    #     query = f"INSERT INTO Perfiles(id_perfil, medicion_cardio, fecha) values (%s, %s, %s, %s);"
     #
-    # for counter in perfiles:
-    #      query = (f"INSERT INTO Perfiles(id_perfil, nombre, apellido, edad) values (")
-    #      query += counter
-    #      print(query)
-    #      cursor.execute(query)
-
-    last_id = [result[0] for result in cursor][-1]
-    limit = input("¿Cuantos usuarios quieres agregar? ")
-    for id_perfil in range(last_id+1, last_id+limit):
-        system('clear')
-        name = input("Introduzca su nombre: ")
-        apellido = input("Introduzca su apellido: ")
-        edad = input("Introduzca su edad: ")
-        query_data = (id_perfil, name, apellido, edad)
-        query = f"INSERT INTO Perfiles(id_perfil, nombre, apellido, edad) values (%s, %s, %s, %s);"
-
-        cursor.execute(query, query_data)
-
-    last_id = [result[0] for result in cursor][-1]
-    limit = input("¿Cuantos usuarios quieres agregar? ")
-    for id_perfil in range(last_id+1, last_id+limit):
-        system('clear')
-        name = input("Introduzca su nombre: ")
-        apellido = input("Introduzca su apellido: ")
-        edad = input("Introduzca su edad: ")
-        query_data = (id_perfil, name, apellido, edad)
-        query = f"INSERT INTO Perfiles(id_perfil, medicion_cardio, fecha) values (%s, %s, %s, %s);"
-
-        cursor.execute(query, query_data)
+    #     cursor.execute(query, query_data)
 
 
 
@@ -69,6 +88,7 @@ except mysql.connector.Error as err:
   elif err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
     print("Database does not exist")
   else:
+    print("Error:")
     print(err)
 
 finally:
